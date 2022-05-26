63b0296efe87c50f83637b1db5cb50ed0ee348bd
Joakim Skoglund <joakim@voltura.se>
2022-05-26 16:38:30 +0200

    vs code do not handle global usings

--- BlazorAppWeatherDemo/Program.cs
+++ BlazorAppWeatherDemo/Program.cs
@@ -1,6 +1,18 @@
 using BlazorAppWeatherDemo.Data;
+using BlazorAppWeatherDemo.Pages;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using BlazorAppWeatherDemo;
+using BlazorAppWeatherDemo.Shared;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
