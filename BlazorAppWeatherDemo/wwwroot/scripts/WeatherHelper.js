56f14d606ebde318a698282d849b2b0aa8492c76
Joakim Skoglund <joakim@voltura.se>
2022-05-28 14:38:33 +0200

    Cleanup

---
+++ BlazorAppWeatherDemo/wwwroot/scripts/WeatherHelper.js
@@ -0,0 +1,25 @@
+﻿async function success(pos) {
+    var crd = pos.coords;
+    const weatherData = await WeatherHelper.dotNetObject.invokeMethodAsync('GetWeatherData', crd.latitude, crd.longitude);
+    document.getElementById(`weatherInfo`).innerText = weatherData;
+}
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+class WeatherHelper {
+    static dotNetObject;
+
+    static getCurrentCoordinates(dotNetObjectRef) {
+        WeatherHelper.dotNetObject = dotNetObjectRef;
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+        };
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+}
+
+window.WeatherHelper = WeatherHelper;
